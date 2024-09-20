f774d8d25207f96408f7438595aeb9bef9ed3c3d
Dawson-Bruce <brucedj@mail.uc.edu>
2024-09-20 02:21:28 -0400

    Completed Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+        double initialBalance = 5000;
+        System.out.println("Initial credit card balance: $" + initialBalance);
+
+        double firstMonthBalance = initialBalance * 1.17;
+        System.out.println("Credit card balance after 1 month: $" + firstMonthBalance);
+
+        double secondMonthBalance = firstMonthBalance * 1.17;
+        System.out.println("Credit card balance after 2 months: $" + secondMonthBalance);
     }
 }
